c3b09655c6ea9452ef356c6fdd15cf28e967f373
Albin158 <70146013+Albin158@users.noreply.github.com>
2021-03-29 15:03:30 +0200

    Create temperatur.py

---
+++ temperatur.py
@@ -0,0 +1,11 @@
+Temperaturer = [12,13,13,14,14,15,16,17,17,18,19,20,21,21,22,23,23]
+
+print ( Temperaturer[16] )
+amount = sum(Temperaturer)
+print (amount) #Se totala graderna av alla 16 dagar
+avg = amount/16
+print(avg) #Medeltemperaturen
+#Medeltempen är 18.625 C
+
+
+
